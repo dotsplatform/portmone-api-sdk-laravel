b5f5de15a27b5d69cdd651c84d12eb9fe88d8b80
Oleksandr Polosmak <olexandr.polosmak123@gmail.com>
2024-05-10 14:12:40 +0300

    Handler error responses

---
+++ src/App/Client/Resources/Consts/ResponseStatus.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Description of ResponseStatus.php
+ * @copyright Copyright (c) DOTSPLATFORM, LLC
+ * @author    Oleksandr Polosmak <o.polosmak@dotsplatform.com>
+ */
+
+namespace Dots\Portmone\App\Client\Resources\Consts;
+
+
+class ResponseStatus
+{
+
+}
